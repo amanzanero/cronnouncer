83aa3059fa21c6b35680817397e4b9ed63d71fb7
Andrew Manzanero <manzanero.andrew@gmail.com>
2020-12-07 21:57:29 -0800

    add error logging

--- src/handlers/message/index.ts
+++ src/handlers/message/index.ts
@@ -1,10 +1,10 @@
 import { Message } from "discord.js";
 import * as messageHandlers from "./message";
 
-export default function handleMessages(message: Message): void {
+export default async function handleMessages(message: Message): Promise<void> {
   switch (message.content) {
     case "!ping":
-      messageHandlers.handlePing(message);
+      await messageHandlers.handlePing(message);
       break;
     default:
       break;
--- src/handlers/message/message.ts
+++ src/handlers/message/message.ts
@@ -4,7 +4,11 @@ This file contains handlers for messages
 import { Message } from "discord.js";
 import { logger } from "../../services";
 
-export function handlePing(message: Message): void {
-  message.channel.send("pong!🏓");
-  logger.info(`[!ping] user: ${message.author} sent: ${message.createdAt}`);
+export async function handlePing(message: Message): Promise<void> {
+  try {
+    await message.channel.send("pong!🏓");
+    logger.info(`[!ping] user: ${message.author} sent: ${message.createdAt}`);
+  } catch (e) {
+    logger.error(`[!ping] ${e.stack}`);
+  }
 }
