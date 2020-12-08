e2c2909bae14e01e15e14e4e1fc92588ab1e6f55
Andrew Manzanero <manzanero.andrew@gmail.com>
2020-12-08 13:57:22 -0800

    add testing suite

--- src/handlers/message/index.ts
+++ src/handlers/message/index.ts
@@ -1,9 +1,14 @@
 import { Message } from "discord.js";
 import * as messageHandlers from "./message";
 
+import { parseCommand } from "../parser";
+
 export default async function handleMessages(message: Message): Promise<void> {
-  switch (message.content) {
-    case "!ping":
+  const { content: incoming } = message;
+  const command = parseCommand(incoming);
+
+  switch (command) {
+    case "ping":
       await messageHandlers.handlePing(message);
       break;
     default:
