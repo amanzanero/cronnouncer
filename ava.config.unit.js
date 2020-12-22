dc1e06f6a3d8732b148435107face62ac346ca2d
Andrew Manzanero <manzanero.andrew@gmail.com>
2020-12-21 19:03:37 -0800

    ignore mocks in tests

--- ava.config.unit.js
+++ ava.config.unit.js
@@ -1,5 +1,5 @@
 export default {
   extensions: ["ts"],
-  files: ["test/unit/**/*.ts", "!test/test_utils", "!test/e2e"],
+  files: ["test/unit/**/*.ts", "!test/**/mocks", "!test/test_utils", "!test/e2e"],
   require: ["ts-node/register"],
 };
