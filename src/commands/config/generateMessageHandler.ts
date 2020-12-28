d68933824bc2e68facf219b89d21aaeb75d26615
Andrew Manzanero <manzanero.andrew@gmail.com>
2020-12-28 12:57:25 -0800

    handle guild only commands

--- src/commands/config/generateMessageHandler.ts
+++ src/commands/config/generateMessageHandler.ts
@@ -7,15 +7,11 @@ import { UNKNOWN_COMMAND_RESPONSE } from "../index";
 export function generateMessageHandler(client: Client, commands: CommandMap) {
   return async (message: Message) => {
     if (message.author.bot) return; // we dont fuk w bots
-
-    /**
-     * Will add setting stuff here
-     */
-
     if (!isCommand(message.content)) return;
+
     const { command, args } = parseCommand(message.content);
 
-    // handle wrong command
+    // handle invalid command
     const cmd = commands[command];
     if (!cmd) {
       try {
@@ -26,10 +22,18 @@ export function generateMessageHandler(client: Client, commands: CommandMap) {
       return;
     }
 
-    // execute actual command
     logger.info(
       `[CMD] ${message.author.username} (${message.author.id}) ran command ${cmd.help.name}`,
     );
+
+    const invalidGuildCommand = cmd.conf.guildOnly && !message.guild;
+    if (invalidGuildCommand) {
+      await message.channel.send(
+        `The command \`${cmd.help.name}\` can only be used from within a server.`,
+      );
+      return;
+    }
+
     try {
       await cmd.execute(client, message, args);
     } catch (e) {
